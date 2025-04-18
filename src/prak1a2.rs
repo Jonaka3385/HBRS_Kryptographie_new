d7043d158358e6f185ef824fd2bfc2e811e4b220
Jonas Kawohl <j.kawohl@icloud.com>
2025-04-18 17:47:24 +0200

    prak1a1 und prak1a2 update

--- src/prak1a2.rs
+++ src/prak1a2.rs
@@ -9,9 +9,6 @@ struct BigPair {
 pub fn run() {
     let p_length = 3072_usize;
     let q_length = 256_usize;
-    let modifier = 7_usize; // everything smaller than 7 takes way too long
-    // 7 -> 1 403 584 rounds fuer 3072 bit
-    // 6 -> 23 548 233 345 728 rounds fuer 3072 bit
     let pair = gen_dsa_p_q(q_length, p_length);
     let p = pair.p;
     let q = pair.q;
